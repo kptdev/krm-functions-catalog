d57d0b4830e9790634e53ba0d0b857e834e293cc
Mózes László Máté <laszlo.mozes@nokia.com>
2026-04-24 15:31:56 +0200

    bump versions higher, add Dockerfile caching
    Signed-off-by: Mózes László Máté <laszlo.mozes@nokia.com>

--- functions/go/create-setters/go.mod
+++ functions/go/create-setters/go.mod
@@ -1,6 +1,6 @@
 module github.com/kptdev/krm-functions-catalog/functions/go/create-setters
 
-go 1.25.7
+go 1.25.9
 
 require (
 	github.com/stretchr/testify v1.11.1
--- functions/go/ensure-name-substring/ensure_name_substring.go
+++ functions/go/ensure-name-substring/ensure_name_substring.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -140,6 +140,8 @@ func (ens *EnsureNameSubstring) Transform(m resmap.ResMap) error {
 				fltr = prefix.Filter{FieldSpec: fs, Prefix: ens.Substring}
 			case Append:
 				fltr = suffix.Filter{FieldSpec: fs, Suffix: ens.Substring}
+			default:
+				return fmt.Errorf("unknown edit mode: %s", ens.EditMode)
 			}
 			err = r.ApplyFilter(fltr)
 			if err != nil {
